c577a0c0a058cb80642cc333ad0318bd4397b05a
itsvoit <wojciech.skrzypiec2@gmail.com>
2021-11-23 19:27:41 +0100

    Updated Prostokat and fixed usage in Main

--- scr/com/voit/Main.java
+++ scr/com/voit/Main.java
@@ -3,6 +3,6 @@ package com.voit;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
-        Prostokat prostokat1 = new Prostokat(1, 1);
+        Prostokat prostokat1 = new Prostokat(1, 1, 2, 2);
     }
 }
