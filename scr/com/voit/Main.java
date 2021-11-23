9c8cd5699be379bf5b7aca59b274e2fa2653867c
itsvoit <wojciech.skrzypiec2@gmail.com>
2021-11-23 19:24:46 +0100

    Added class Prostokat Added usage of Prostokat in Main

--- scr/com/voit/Main.java
+++ scr/com/voit/Main.java
@@ -3,5 +3,6 @@ package com.voit;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
+        Prostokat prostokat1 = new Prostokat(1, 1);
     }
 }
---
+++ scr/com/voit/Prostokat.java
@@ -0,0 +1,10 @@
+package com.voit;
+
+public class Prostokat {
+    private int x;
+    private int y;
+    Prostokat (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
